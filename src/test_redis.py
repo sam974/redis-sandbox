8ae27860863e72cf54a1b044cbd8acb5dc668a31
Samuel Judith <sjudith@sierrawireless.com>
2020-10-16 18:00:33 +0200

    Get response

--- src/test_redis.py
+++ src/test_redis.py
@@ -4,4 +4,5 @@ def test_record():
     print(f"Mi jouer ek redis ")
     recorder_instance = redis.Redis(host="127.0.0.1", port=6379, db=0)
     recorder_instance.rpush("toto", "i marche sa")
-
+    response_debug = recorder_instance.lpop("toto")
+    print(f"DEBUG RESP={response_debug}")

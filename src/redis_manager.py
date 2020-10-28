5b1a19ff4f5d095010185600a03bc74586631545
Samuel Judith <sjudith@sierrawireless.com>
2020-10-28 09:42:10 +0100

    split file

---
+++ src/redis_manager.py
@@ -0,0 +1,12 @@
+import redis
+import pickle
+
+def play_with_redis():
+    print(f"Mi jouer ek redis ")
+    recorder_instance = redis.Redis(host="127.0.0.1", port=6379, db=0)
+    recorder_instance.rpush("toto", "i marche sa")
+    response_debug = recorder_instance.lpop("toto")
+    print(f"DEBUG RESP={response_debug}")
+    packed_object = recorder_instance.lpop("DUT7")
+    response = pickle.loads(packed_object)
+    print(f"Replayed response: {response}")
--- src/test_redis.py
+++ src/test_redis.py
@@ -1,13 +1,6 @@
-import redis
-import pickle
+import redis_manager
 
 
 def test_record():
-    print(f"Mi jouer ek redis ")
-    recorder_instance = redis.Redis(host="127.0.0.1", port=6379, db=0)
-    recorder_instance.rpush("toto", "i marche sa")
-    response_debug = recorder_instance.lpop("toto")
-    print(f"DEBUG RESP={response_debug}")
-    packed_object = recorder_instance.lpop("DUT7")
-    response = pickle.loads(packed_object)
-    print(f"Replayed response: {response}")
+    print(f"Mi jouer ek redis depuis test_redis ")
+    redis_manager.play_with_redis()
